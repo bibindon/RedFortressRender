2ed9d014e4aaadef8fe9da4285b5f08be99de122
bibindon <bibindon0814@gmail.com>
2025-09-21 21:24:28 +0900

    s

--- Render/MeshInstancing.cpp
+++ Render/MeshInstancing.cpp
@@ -85,7 +85,7 @@ void MeshInstancing::Initialize()
             for (int h = 0; h < H; h++)
             {
                 int e = H * W * d + h * W + w;
-                worldPos[e].x = 10.f * (w - (W / 2.f));
+                worldPos[e].x = (float)(10.f * (w - (W / 2.f)));
                 worldPos[e].y = 10.f * (h - (H / 2.f));
                 worldPos[e].z = 10.f * (d - (D / 2.f));
             }
