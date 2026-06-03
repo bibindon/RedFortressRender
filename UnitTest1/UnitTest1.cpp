620d5fd0663ccdcd600a47cbb512dbe56a74a1bb
bibindon <bibindon0814@gmail.com>
2026-06-03 11:11:46 +0900

    s

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -7,14 +7,57 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {
+    namespace
+    {
+        bool FileExists(const std::wstring& filePath)
+        {
+            const DWORD attributes = GetFileAttributesW(filePath.c_str());
+            if (attributes == INVALID_FILE_ATTRIBUTES)
+            {
+                return false;
+            }
+
+            return (attributes & FILE_ATTRIBUTE_DIRECTORY) == 0;
+        }
+
+        std::wstring GetWolf2FilePath(const std::wstring& fileName)
+        {
+            wchar_t currentDirectory[MAX_PATH] { };
+            const DWORD length = GetCurrentDirectoryW(_countof(currentDirectory), currentDirectory);
+            if (length == 0 || length >= _countof(currentDirectory))
+            {
+                return L"Sample\\res\\model\\wolf2\\" + fileName;
+            }
+
+            std::wstring directory(currentDirectory);
+            for (int i = 0; i < 8; ++i)
+            {
+                const std::wstring candidate = directory + L"\\Sample\\res\\model\\wolf2\\" + fileName;
+                if (FileExists(candidate))
+                {
+                    return candidate;
+                }
+
+                const std::size_t slashPos = directory.find_last_of(L"\\/");
+                if (slashPos == std::wstring::npos)
+                {
+                    break;
+                }
+
+                directory = directory.substr(0, slashPos);
+            }
+
+            return L"Sample\\res\\model\\wolf2\\" + fileName;
+        }
+    }
+
     TEST_CLASS(CustomXLoaderTests)
     {
     public:
 
         TEST_METHOD(LoadWolfMeshFrameHierarchy)
         {
-            const std::wstring filePath =
-                L"C:\\Users\\bibindon\\source\\repos\\bibindon\\RedfortressRender\\Sample\\res\\model\\wolf2\\wolfAnim.x";
+            const std::wstring filePath = GetWolf2FilePath(L"wolfAnim.x");
 
             const NSRender::CustomXFrameHierarchyLoadResult result =
                 NSRender::LoadCustomXFrameHierarchyForTest(filePath);
@@ -26,8 +69,7 @@ namespace UnitTest1
 
         TEST_METHOD(LoadWolfRunAnimationFrameHierarchy)
         {
-            const std::wstring filePath =
-                L"C:\\Users\\bibindon\\source\\repos\\bibindon\\RedfortressRender\\Sample\\res\\model\\wolf2\\wolfAnim.run.x";
+            const std::wstring filePath = GetWolf2FilePath(L"wolfAnim.run.x");
 
             const NSRender::CustomXFrameHierarchyLoadResult result =
                 NSRender::LoadCustomXFrameHierarchyForTest(filePath);
