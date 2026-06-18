9cdd30f1b9637a2395b512a9968924d9d83d2f92
bibindon <bibindon0814@gmail.com>
2026-06-18 10:57:20 +0900

    s

--- Render/GBuffer.cpp
+++ Render/GBuffer.cpp
@@ -181,7 +181,7 @@ void GBuffer::Draw(const std::deque<MeshMixManager>& meshList,
     hr = Common::D3DDevice()->Clear(0,
                                     NULL,
                                     D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
-                                    D3DCOLOR_RGBA(0, 0, 0, 0),
+                                    D3DCOLOR_XRGB(255, 0, 0),
                                     1.0f,
                                     0);
 
